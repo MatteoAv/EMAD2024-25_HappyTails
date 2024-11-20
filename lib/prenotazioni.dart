6e579808c3679bd2cedc048ac229223070928849
MatteoAv <matteoavella8@gmail.com>
2024-11-20 21:55:11 +0100

    Pagina PrenotazioniGuest

--- lib/prenotazioni.dart
+++ lib/prenotazioni.dart
@@ -12,6 +12,110 @@ class PrenotazioniPage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Header(), // Aggiungi il widget Header in cima
+
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            height: 610,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 150, 142, 125),
+              borderRadius: BorderRadius.circular(10), // Arrotonda gli angoli del Container
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+
+                  //Testo
+                  const Text(
+                    'Per visualizzare le prenotazioni in corso accedi con il tuo account oppure registrati',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 20, fontWeight: FontWeight.bold
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  // Primo blocco: Immagine e bottone
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10), // Arrotonda gli angoli dell'immagine
+                    child: Image.asset(
+                      'assets/pawShaking.png', 
+                      //dopo aver caricato un'immagine nella cartella assets e aver aggiornato il pubspec.yaml eseguire il comando flutter pub get
+                      width: double.infinity, // Aggiunge larghezza per riempire il container
+                      height: 190, // Imposta un'altezza per l'immagine
+                      fit: BoxFit.cover, // Imposta il tipo di ritaglio dell'immagine
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child:                      
+                        ElevatedButton(
+                          onPressed: () {
+                            // Azione per il pulsante 
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.orange, // Colore arancione
+                            foregroundColor: Colors.white, // Testo bianco
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Angoli arrotondati
+                            ),
+                          ),
+                          child: const Text('Accedi'),
+                        )
+                      )
+
+                    ],
+                  ),
+                  
+                  const SizedBox(height: 20), // Spaziatura tra i due blocchi
+
+                  // Secondo blocco: Immagine e bottone 
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10), // Arrotonda gli angoli dell'immagine
+                    child: Image.asset(
+                      'assets/puppies.png', 
+                      //dopo aver caricato un'immagine nella cartella assets e aver aggiornato il pubspec.yaml eseguire il comando flutter pub get
+                      width: double.infinity, // Aggiunge larghezza per riempire il container
+                      height: 190, // Imposta un'altezza per l'immagine
+                      fit: BoxFit.cover, // Imposta il tipo di ritaglio dell'immagine
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child:                      
+                        ElevatedButton(
+                          onPressed: () {
+                            // Azione per il pulsante 
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.orange, // Colore arancione
+                            foregroundColor: Colors.white, // Testo bianco
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10), // Angoli arrotondati
+                            ),
+                          ),
+                          child: const Text('Registrati'),
+                        )
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          /*
           Expanded(
             child: Center(
               child: Column(
@@ -24,7 +128,7 @@ class PrenotazioniPage extends StatelessWidget {
                   ),
                   const SizedBox(height: 20), // Spaziatura tra il testo e i pulsanti
                   SizedBox(
-                    width: 400, // Larghezza uniforme per i pulsanti
+                    width: 350, // Larghezza uniforme per i pulsanti
                     child: ElevatedButton(
                       onPressed: () {
                         // Azione per il pulsante Accedi
@@ -41,7 +145,7 @@ class PrenotazioniPage extends StatelessWidget {
                   ),
                   const SizedBox(height: 10), // Spaziatura tra i due pulsanti
                   SizedBox(
-                    width: 400, // Larghezza uniforme per i pulsanti
+                    width: 350, // Larghezza uniforme per i pulsanti
                     child: ElevatedButton(
                       onPressed: () {
                         // Azione per il pulsante Registrati
@@ -59,7 +163,7 @@ class PrenotazioniPage extends StatelessWidget {
                 ],
               ),
             ),
-          ),
+          ),*/
         ],
       ),
     );
