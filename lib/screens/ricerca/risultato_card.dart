ebd7dd3ae3bceb6c8d20b29887c847e47530d1b6
sasibasi <pinco265@gmail.com>
2024-11-22 00:53:56 +0100

    rating

--- lib/screens/ricerca/risultato_card.dart
+++ lib/screens/ricerca/risultato_card.dart
@@ -59,7 +59,7 @@ class VerticalCard extends StatelessWidget {
                         children: List.generate(
                           5, 
                           (index) => Icon(
-                            index < item.rating ? Icons.star : Icons.star_border,
+                            index < 4 ? Icons.star : Icons.star_border,
                             color: Colors.amber,
                             size: 20,
                           ),
