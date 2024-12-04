6748a25b90c20ecb171b3b868b6c331cf1915467
Giodvk <giovannicasaburi02@gmail.com>
2024-12-04 15:24:40 +0100

    Update Profile Page and optimization

--- lib/UserManage/screens/profile_page.dart
+++ lib/UserManage/screens/profile_page.dart
@@ -1,9 +1,36 @@
+/* Pagina del profilo dell'utente loggato
+UserAsync usa l'user provider per aggiornare le informazioni riguardanti l'utente,
+se il provider si aggiorna esso notificherà automaticamente l'UserAsync
+
+PetsAsync usa il pet provider per aggiornare la lista dei pet posseduti dall'utente,
+se un utente aggiunge un pet con il Floating Action Button, il provider notificherà le modifiche e
+petAsync si aggiorna di conseguenza
+
+bookingAsync usa il book provider per aggiornare le liste delle prenotazioni in corso ed effettuate
+dall'utente, se l'utente effettua o cancella una prenotazione, il book provider notificherà le modifiche
+e di conseguenza bookAsync si aggiornerà
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:happy_tails/UserManage/repositories/local_database.dart';
 import 'package:happy_tails/UserManage/widgets/expandable_button.dart';
 import 'package:happy_tails/app/routes.dart';
 import 'package:happy_tails/UserManage/providers/profile_providers.dart';
+import 'package:happy_tails/UserManage/widgets/PetCard.dart';
 
 class UserProfilePage extends ConsumerWidget {
   const UserProfilePage({Key? key}) : super(key: key);
@@ -14,9 +41,8 @@ class UserProfilePage extends ConsumerWidget {
     final petsAsync = ref.watch(petsProvider);
     final bookingsAsync = ref.watch(bookingsProvider);
     final isPetsTabSelected = ref.watch(tabSelectionProvider);
-    final addPetsate = ref.watch(addPetProvider);
+    
 
-    int user_id;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -25,68 +51,76 @@ class UserProfilePage extends ConsumerWidget {
         ),
         backgroundColor: Colors.deepOrange[50],
       ),
-      body: Padding(
+      body: CustomScrollView(
+  slivers: [
+    // Sliver: Intestazione (Informazioni sull'utente)
+    SliverToBoxAdapter(
+      child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // User Info Section
-            userAsync.when(
-              data: (user) => Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+        child: userAsync.when(
+          data: (user) => Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: NetworkImage(user?.imageUrl ?? ''),
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundImage: NetworkImage(user?.imageUrl ?? ''),
+                  const Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrange,
+                    ),
                   ),
-                  const SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome',
-                        style: const TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepOrange,
-                        ),
-                      ),
-                      Text(
-                        user?.userName ?? 'Guest',
-                        style: const TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepOrange,
-                      ),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'City: ${user?.citta ?? 'Unknown City'}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    user?.userName ?? 'Guest',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrange,
+                    ),
                   ),
-                  const Spacer(),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, AppRoutes.settings);
-                    },
-                    child: const Icon(
-                      Icons.settings,
-                      color: Colors.black,
-                      size: 30,
+                  const SizedBox(height: 8),
+                  Text(
+                    'City: ${user?.citta ?? 'Unknown City'}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black54,
                     ),
                   ),
                 ],
               ),
-              loading: () => const CircularProgressIndicator(),
-              error: (err, _) => Text('Error: $err'),
-            ),
+              const Spacer(),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, AppRoutes.settings);
+                },
+                child: const Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (err, _) => Text('Error: $err'),
+        ),
+      ),
+    ),
+    // Sliver: Pulsanti di navigazione (Pets e Book)
+    SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             const SizedBox(height: 50),
-            // Expandable Buttons
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -94,139 +128,97 @@ class UserProfilePage extends ConsumerWidget {
                   icon: Icons.pets,
                   label: 'Pets',
                   isExpanded: isPetsTabSelected,
-                  onTap: () => ref.read(tabSelectionProvider.notifier).state = true,
+                  onTap: () =>
+                      ref.read(tabSelectionProvider.notifier).state = true,
                 ),
-                SizedBox(width: 30),
+                const SizedBox(width: 30),
                 ExpandableButton(
                   icon: Icons.calendar_month,
-                  label: 'Bookings',
+                  label: 'Book',
                   isExpanded: !isPetsTabSelected,
-                  onTap: () => ref.read(tabSelectionProvider.notifier).state = false,
+                  onTap: () =>
+                      ref.read(tabSelectionProvider.notifier).state = false,
                 ),
-               
               ],
             ),
             const SizedBox(height: 20),
-             Text(
-                  textAlign: TextAlign.left,
-                  isPetsTabSelected ? 'Lista di Pet' : 'Lista prenotazioni',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-            const SizedBox(height: 24),
-            // Tab content
-            Expanded(
-              child: isPetsTabSelected
-                  ? petsAsync.when(
-                      data: (pets) => ListView.builder(
-                        itemCount: pets.length,
-                        itemBuilder: (context, index) {
-                          final pet = pets[index];
-                          return InkWell(
-                            splashColor: Colors.deepOrange[50],
-                            onTap: () {
-                              // Placeholder for action when a pet is tapped
-                            },
-                            child: Container(
-                              height:90,
-                              child: Card(
-                              elevation: 20,
-                              color: Colors.deepOrange[50],
-                              margin: const EdgeInsets.symmetric(vertical: 8),
-                              child: ListTile(
-                                leading: CircleAvatar(
-                                  radius: 40,
-                                  backgroundColor: Colors.deepOrangeAccent,
-                                  child: const Icon(Icons.pets, color: Colors.white, size:40),
-                                ),
-                                title: Text(
-                                  pet.name,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                    ),
-                                ),
-                                subtitle: Text(
-                                  'Type: ${pet.type}',
-                                   style: const TextStyle(
-                                    fontSize: 18
-                                   ),
-                                  ),
-                                trailing: IconButton(
-                                  icon: const Icon(
-                                    Icons.medical_services,
-                                    color: Colors.deepOrangeAccent,
-                                    size: 40,
-                                  ),
-                                  onPressed: () {
-                                    // Placeholder for veterinary record action
-                                  },
-                                ),
-                              ),
-                            ),
-                            )
-                          );
-                        },
-                      ),
-                      loading: () => const CircularProgressIndicator(),
-                      error: (err, _) => Text('Error: $err'),
-                    )
-                  : bookingsAsync.when(
-                      data: (bookings) => ListView.builder(
-                        itemCount: bookings.length,
-                        itemBuilder: (context, index) {
-                          final booking = bookings[index];
-                          return InkWell(
-                            onTap: () {
-                              // Placeholder for action when a booking is tapped
-                            },
-                            child: Container(
-                              height: 135,
-                              child: Card(
-                              elevation: 20,
-                              color: Colors.deepOrange[50],
-                              margin: const EdgeInsets.symmetric(vertical: 8),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Booking from ${booking.dateBegin} to ${booking.dateEnd}',
-                                      style: const TextStyle(
-                                        fontSize: 19,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text('Price: ${booking.price.toStringAsFixed(2)}€',
-                                    style: TextStyle(
-                                      fontSize: 18
-                                    ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text('Status: ${booking.state}',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      color: booking.state == 'Confermata' ? Colors.green : Colors.red
-                                    ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            )
-                          );
-                        },
-                      ),
-                      loading: () => const CircularProgressIndicator(),
-                      error: (err, _) => Text('Error: $err'),
-                    ),
+            Text(
+              isPetsTabSelected ? 'Lista di Pet' : 'Lista prenotazioni',
+              style: const TextStyle(fontSize: 30),
             ),
           ],
         ),
       ),
+    ),
+    // Sliver: Contenuto Tab (Lista di Pets o Lista Prenotazioni)
+    SliverFillRemaining(
+      child: isPetsTabSelected
+          ? petsAsync.when(
+              data: (pets) => ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.only(top: 16.0),
+                itemCount: pets.length,
+                itemBuilder: (context, index) {
+                  final pet = pets[index];
+                  return PetCard(petName: pet.name, petType: pet.type);
+                },
+              ),
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (err, _) => Center(child: Text('Error: $err')),
+            )
+          : bookingsAsync.when(
+              data: (bookings) => ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.only(top: 16.0),
+                itemCount: bookings.length,
+                itemBuilder: (context, index) {
+                  final booking = bookings[index];
+                  return Container(
+                    height: 135,
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Card(
+                      elevation: 20,
+                      color: Colors.deepOrange[50],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Book from ${booking.dateBegin} to ${booking.dateEnd}',
+                              style: const TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              'Price: ${booking.price.toStringAsFixed(2)}€',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              'Status: ${booking.state}',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: booking.state == 'Confermata'
+                                    ? Colors.green
+                                    : Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (err, _) => Center(child: Text('Error: $err')),
+            ),
+    ),
+  ],
+),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -234,7 +226,7 @@ class UserProfilePage extends ConsumerWidget {
           builder : (context) => const AddPetDialog());
           },
         child: const Icon(Icons.add),
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.orange,
       ),
     );
   }
