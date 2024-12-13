7003e542e1fcdeb41feda49106ea7ea1f7469c3b
MatteoAv <matteoavella8@gmail.com>
2024-12-13 14:48:34 +0100

    Porfilo con recensioni

--- lib/screens/ricerca/petsitter_page.dart
+++ lib/screens/ricerca/petsitter_page.dart
@@ -17,11 +17,58 @@ class _ProfiloPetsitterState extends State<ProfiloPetsitter> {
   TimeOfDay? startTime;
 
   List<Map<String, dynamic>> reviews = [];
+  double totalRating = 0;  // Variabile per il punteggio totale
+  int totalReviews = 0;    // Variabile per il numero di recensioni
 
   @override
   void initState() {
     super.initState();
     _loadReviews(); // Call _loadReviews here
+  }
+
+
+  // Funzione per selezionare la data di inizio
+  Future<void> _selectStartDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedStartDate ?? DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedStartDate) {
+      setState(() {
+        selectedStartDate = picked;
+        selectedEndDate = null;
+      });
+    }
+  }
+
+  // Funzione per selezionare la data di fine
+  Future<void> _selectEndDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedEndDate ?? selectedStartDate!.add(const Duration(days: 1)),
+      firstDate: selectedStartDate!,
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedEndDate) {
+      setState(() {
+        selectedEndDate = picked;
+      });
+    }
+  }
+
+  // Funzione per selezionare l'orario di inizio
+  Future<void> _selectStartTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: startTime ?? TimeOfDay.now(),
+    );
+    if (picked != null && picked != startTime) {
+      setState(() {
+        startTime = picked;
+      });
+    }
   }
 
   Future<void> _loadReviews() async {
@@ -46,16 +93,18 @@ class _ProfiloPetsitterState extends State<ProfiloPetsitter> {
             // Aggiungi il controllo per verificare se i campi sono corretti
             print('Review data: $review');
             return {
-              'data': review['date_end'] ?? 'Sconosciuto',  // Usa un valore di fallback se 'dateEnd' è null
-              'recensione': review['review'] ?? 'Sconosciuto',  // Usa un valore di fallback se 'review' è null
-              'voto': review['vote'] ?? 0,  // Usa un valore di fallback se 'vote' è null
-              'proprietario': review['owner_name'] ?? 'Sconosciuto',  // Usa un valore di fallback se 'owner_id' è null
-              'profilePicture': review['profile_picture'] ?? 'assets/default_profile.png',  // Usa un valore di fallback per 'profile_picture'
+              'data': review['date_end'] ?? 'Sconosciuto',
+              'recensione': review['review'] ?? 'Sconosciuto',
+              'voto': review['vote'] ?? 0,
+              'proprietario': review['owner_name'] ?? 'Sconosciuto',
+              'profilePicture': review['profile_picture'] ?? 'assets/default_profile.png',
             };
           }).toList();
 
           setState(() {
-            reviews = reviewsList; // Aggiorna la lista delle recensioni
+            reviews = reviewsList;
+            totalReviews = reviewsList.length;
+            totalRating = reviewsList.fold(0, (sum, review) => sum + (review['voto'] ?? 0));
           });
         } else {
           print('Errore: la risposta della query non è una lista');
@@ -66,13 +115,26 @@ class _ProfiloPetsitterState extends State<ProfiloPetsitter> {
     }
   }
 
+  double get averageRating {
+    if (totalReviews == 0) {
+      return 0;
+    }
+    // Media dei voti su una scala da 1 a 10
+    return totalRating / totalReviews / 2; // Diviso per 2 per trasformare da scala 1-10 a scala 1-5 stelle
+  }
+
   @override
   Widget build(BuildContext context) {
     final PetSitter petsitter = widget.petsitter;
     final int petsitterId = petsitter.id;
 
-
     print("ID del petsitter PETSITTER_PAGE: $petsitterId");
+
+    
+
+    // Calcoliamo il numero di stelle piene, metà stelle e stelle vuote
+    int fullStars = averageRating.floor();
+    bool hasHalfStar = averageRating - fullStars >= 0.5;
 
     return Scaffold(
       appBar: AppBar(
@@ -93,10 +155,225 @@ class _ProfiloPetsitterState extends State<ProfiloPetsitter> {
             children: [
               const SizedBox(height: 16),
 
+              // Sezione del profilo
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/puppies.png'),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Nome: Giovanni Rossi',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Città: Roma',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+
+              // Form di prenotazione
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Prenota il pet-sitter per i giorni di cui hai bisogno, seleziona data e ora e conferma la prenotazione',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          const Icon(Icons.calendar_today, color: Colors.black),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () => _selectStartDate(context),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.orange,
+                                      foregroundColor: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      selectedStartDate == null
+                                          ? 'Data Inizio'
+                                          : '${selectedStartDate!.day}/${selectedStartDate!.month}/${selectedStartDate!.year}',
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () => _selectEndDate(context),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.orange,
+                                      foregroundColor: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      selectedEndDate == null
+                                          ? 'Data Fine'
+                                          : '${selectedEndDate!.day}/${selectedEndDate!.month}/${selectedEndDate!.year}',
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          const Icon(Icons.access_time, color: Colors.black),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () => _selectStartTime(context),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                              child: Text(
+                                startTime == null
+                                    ? 'Orario di inizio'
+                                    : '${startTime!.hour}:${startTime!.minute.toString().padLeft(2, '0')}',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: const Text('Prenotazione Confermata'),
+                              content: const Text('La tua prenotazione è stata effettuata con successo!'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          minimumSize: const Size(double.infinity, 50),
+                        ),
+                        child: const Text('Prenota'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Valutazione complessiva
+              Center(
+                child: Column(
+                  children: [
+                    const Text(
+                      'Valutazione Complessiva',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      averageRating.toStringAsFixed(1),
+                      style: const TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        5,
+                        (index) {
+                          if (index < fullStars) {
+                            return const Icon(Icons.star, color: Colors.orange, size: 28);
+                          } else if (index == fullStars && hasHalfStar) {
+                            return const Icon(Icons.star_half, color: Colors.orange, size: 28);
+                          } else {
+                            return const Icon(Icons.star_border, color: Colors.orange, size: 28);
+                          }
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Basato su $totalReviews recensioni',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
               // Recensioni
-              const SizedBox(height: 16),
+              const SizedBox(height: 25),
               Column(
                 children: reviews.map((review) {
+                  double rating = (review['voto'] ?? 0) / 2;  // Adattiamo il voto da 1-10 a 1-5 stelle
+
+                  // Calcoliamo il numero di stelle piene, metà stelle e stelle vuote
+                  int fullStars = rating.floor();
+                  bool hasHalfStar = rating - fullStars >= 0.5;
+
                   return Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: Row(
@@ -132,9 +409,9 @@ class _ProfiloPetsitterState extends State<ProfiloPetsitter> {
                                 children: List.generate(
                                   5,
                                   (index) => Icon(
-                                    index < (review['voto'] ?? 0)  // Usa 0 se 'voto' è null
+                                    index < fullStars
                                         ? Icons.star
-                                        : Icons.star_border,
+                                        : (index == fullStars && hasHalfStar ? Icons.star_half : Icons.star_border),
                                     color: Colors.orange,
                                     size: 20,
                                   ),
