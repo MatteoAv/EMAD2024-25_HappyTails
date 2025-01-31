826432596603a0b9a1777bfd5c65086a3ab627a4
Giovanni Casaburi <giovannicasaburi02@gmail.com>
2025-01-31 19:19:34 +0100

    Stripe

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ Future<void> main() async{
   // Configura la chiave pubblica di Stripe
   Stripe.publishableKey = "pk_test_51QnI64HHgxdC6vSSMHcpNZYlfGNYojbojvLWrJVgUX8Uoy1IvuKOTNHUfeJcZw6OJqIt3xxwJyR5sEVuPRsMmyn7000Kl0wG14";
   final serverController = ServerController();
-  serverController.startServer();
+  //serverController.startServer();
   //await LocalDatabase.instance.deleteDatabaseFile();
   await LocalDatabase.instance.database;
   runApp(const ProviderScope(child: MyApp()));
